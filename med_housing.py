dda6426f5cb559de03d38c24b09b690f1b79bcca
zionc <zchilagan678@gmail.com>
2024-07-02 20:14:15 -0400

    housing data

--- med_housing.py
+++ med_housing.py
@@ -1,6 +1,22 @@
 import sys
 assert sys.version_info >= (3,7) # Python 3.7 or above
-
 import sklearn
+from pathlib import Path
+import pandas as pd
+import tarfile
+import urllib.request
 
-print("Hello World!")
+
+def load_housing_data() -> pd.DataFrame:
+    path = Path("datasets/housing.tgz")
+    if not path.is_file():
+        Path("datasets").mkdir(parents=True, exist_ok=True)
+        url = "https://github.com/ageron/data/raw/main/housing.tgz"
+        urllib.request.urlretrieve(url, path)
+        with tarfile.open(path) as tar_path:
+            tar_path.extractall(path="datasets")
+    return pd.read_csv(Path("datasets/housing/housing.csv"))
+
+if __name__ == "__main__":
+    housing = load_housing_data()
+    print(housing.head())
